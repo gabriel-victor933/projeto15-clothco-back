53b83618b8e337946b9b23397bdc35544b41eb37
gabriel-victor933 <gabrielvictor933@usp.br>
2023-04-28 09:39:10 -0300

    fixing .env bug

--- src/app.js
+++ src/app.js
@@ -1,11 +1,15 @@
 import router from "./routes/index.route.js";
 import express from "express";
 import cors from "cors";
+import dotenv from "dotenv";
 const server = express();
 
 server.use(express.json());
 server.use(cors());
 server.use(router);
+
+dotenv.config();
+
 
 
 server.listen(process.env.PORT, () => {
