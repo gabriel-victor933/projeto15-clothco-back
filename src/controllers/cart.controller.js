4f557bc966ad8cd12b4dc5b3e2a83dd9369fd70f
gabriel-victor933 <gabrielvictor933@usp.br>
2023-04-29 11:56:54 -0300

    Add authentication middleware to protect routes

--- src/controllers/cart.controller.js
+++ src/controllers/cart.controller.js
@@ -4,14 +4,20 @@ import { ObjectId } from "mongodb"
 
 export async function getCartItem(req,res){
 
-    res.send("get functionando")
+    const token = req.headers?.authorization.replace("Bearer ","")
+
+    try{
+
+        return res.send(user)
+    }catch(err){
+        return res.status(500).send("server error")
+    }
+    
 }
 
 export async function postCartItem(req,res){
 
     const token = req.headers?.authorization.replace("Bearer ","")
-
-    if(!token) return res.status(401).send("missing token")
 
     const {productId, quantity} = req.body
 
@@ -20,17 +26,17 @@ export async function postCartItem(req,res){
     if(quantity < 1) return res.status(400).send("quantity must be greater than zero")
 
     try{
-        const [user] = await db.collection("users").find({token: token}).toArray()
+        const user = await db.collection("users").find({token: token})
         if (!user) return res.status(401).send("user not logged in");
 
-        const [product] = await db.collection("products").find({_id: new ObjectId(productId)}).toArray()
+        const product = await db.collection("products").find({_id: new ObjectId(productId)})
         if (!product) return res.status(401).send("product not found");
 
         const order = {productId, quantity}
 
-        await db.collection("users").updateOne(user,  { $push: {cart: order} } )
+        const result = await db.collection("users").updateOne({token},  { $push: {cart: order} } )
 
-        return res.status(201).send("added to cart")
+        return res.status(201).send(result)
 
     }catch(err){
 
--- src/routes/cart.route.js
+++ src/routes/cart.route.js
@@ -1,9 +1,10 @@
 import { Router } from "express";
 import {getCartItem, postCartItem} from "../controllers/cart.controller.js"
+import auth from "../middlewares/auth.middleware.js";
 
 const router = Router()
 
-router.get("/cart",getCartItem)
-router.post("/cart",postCartItem)
+router.get("/cart",auth,getCartItem)
+router.post("/cart",auth,postCartItem)
 
 export default router
